55cf80eb56b47e348f8216ad254f9aece16bcc8f
choy30 <alcls0026@gmail.com>
2021-11-19 05:33:33 +1300

    merge with guy's files

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
-from blog.forms import BoatForm, UpdateBoatForm
-from blog.models import Boat
+from blog.forms import BoatForm, UpdateBoatForm, RentForm
+from blog.models import Boat, Rent
 
 
 class HomeView(ListView):
@@ -31,3 +31,18 @@ class DeleteBoatView(DeleteView):
     model = Boat
     template_name = 'boat/delete_boat.html'
     success_url = reverse_lazy('my-boat')
+class RentBoatView(DetailView,CreateView):
+    model = Boat
+    form_class = RentForm
+    template_name = 'rent/rent_boat.html'
+    success_url = reverse_lazy('rent-success')
+def rent_success(request):
+    return render(request, 'rent/rent_success.html')
+class RentListView(ListView):
+    model = Rent
+    template_name = 'rent/rent_list.html'
+    ordering = ['-id']
+class DeleteRentView(DeleteView):
+    model = Rent
+    template_name = 'rent/rent_delete.html'
+    success_url = reverse_lazy('rent-list')
